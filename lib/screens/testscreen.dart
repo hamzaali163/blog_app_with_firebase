b5bfeadffacb809267a82dfccb3ba8ad967d96ea
hamzaali163 <syedhamzaali059@gmail.com>
2025-01-09 21:20:26 +0500

    test screen created

---
+++ lib/screens/testscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Testscreen extends StatelessWidget {
+  const Testscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
