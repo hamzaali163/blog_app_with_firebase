126e9dd78d75cd8fe305725971ee92fdb181438b
hamzaali163 <syedhamzaali059@gmail.com>
2025-01-09 21:30:36 +0500

    testing screen edited

--- lib/screens/testscreen.dart
+++ lib/screens/testscreen.dart
@@ -5,6 +5,10 @@ class Testscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [Text('Github testing')],
+      ),
+    );
   }
 }
